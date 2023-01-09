a8e21f2e53b2d0c8c9167d8d1a48dfa3473959ce
shrinkray <hello@gregmiller.io>
2023-01-09 15:08:04 -0500

    console.log added to show misleading info in userAgent - Add missing stylesheet

--- examples/navigator/index.js
+++ examples/navigator/index.js
@@ -1,1 +1,8 @@
 
+console.log(navigator.userAgent)
+
+/**
+ * This was a test to see if the navigator object was available in the browser.
+ * What we see is that whatever the userAgent displays it's not very helpful.
+ * We even see that the Device that is shown is wrong; it's showing an Intel machine when I'm driving an M1 today.
+ */
